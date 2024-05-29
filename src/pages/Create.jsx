c1fb5c6a3d10919332f7b02a670467c01bad7ae9
Alan Rocha <141082245+alanrochagomes@users.noreply.github.com>
2024-05-29 19:44:34 +0000

    Criação do `<form>`

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,3 +1,28 @@
 export default function Create() {
-    return <div>Create</div>
+    return (
+    <div>
+        <h1>Criar DevMon</h1>
+        
+        <form>
+            <div>
+                <label htmlFor='nome'>Nome*:</label><br></br>
+                <input type='text' id='nome' name='nome' placeholder='Insira o nome'></input>
+            </div>
+
+            <div>
+                <label htmlFor='Imagem'>Imagem (URL)*:</label><br></br>
+                <input type='text' id='imagem' name='imagem' placeholder='Insira a URL da imagem'></input>
+            </div>
+
+            <div>
+                <label htmlFor='nome'>Evolui Para (opcional):</label><br></br>
+                <input type='text' id='nome' name='nome' placeholder='Insira o nome da evolução'></input>
+            </div>
+
+            <div>
+                <button type='submit'>Criar</button>
+                <button type='reset'>Reiniciar</button>
+            </div>
+        </form>
+    </div>)
 }
